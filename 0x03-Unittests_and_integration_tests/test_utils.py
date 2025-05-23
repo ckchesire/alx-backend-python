ce1817698416210e5a8c99f1c63bbd31c263a422
Christian Chesire <christianck.chesire@gmail.com>
2025-05-23 22:34:20 +0000

    Added test function to verify if KeyError is raised when the path is invalid.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,3 +19,15 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """Test if access_nested_map returns the expected output."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test to see if KeyError is raised when an invalid path is parsed."""
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+        self.assertEqual(
+                str(cm.exception),
+                repr(path[len(cm.exeption.args[0]) - 1]))
